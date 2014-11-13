8e9bf9387c6f544caf63d746d4e60a0c992e86ef
Kyle Gorman <gormanky@ohsu.edu>
2014-11-12 16:44:46 -0800

    incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name="nlup",
-      version="0.1",
+      version="0.2",
       description="Core libraries for natural language processing, with perceptron classifiers",
       author="Kyle Gorman",
       author_email="gormanky@ohsu.edu",
