74491f7a5886a5c306a9d69e325c2316ee06ff7a
Kyle Gorman <kylebgorman@gmail.com>
2015-02-28 10:35:07 -0800

    version number increment

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 
 setup(name="nlup",
-      version="0.3",
-      description="Core libraries for natural language processing, with perceptron classifiers",
+      version="0.4",
+      description="Core libraries for natural language processing",
       author="Kyle Gorman",
       author_email="gormanky@ohsu.edu",
       url="http://github.com/cslu-nlp/nlup/",
-      install_requires=["jsonpickle >= 0.8.0"],
+      install_requires=["jsonpickle >= 0.9.0"],
       packages=["nlup"])
