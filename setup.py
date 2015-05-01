73528db01498b7e66a2006e22655a9495f2c7561
Hobson Lane <hobsonlane@gmail.com>
2015-05-01 12:38:51 -0700

    fix bug and add README.rst

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+description = "Core libraries for natural language processing",
+
+# To keep the docs pretty, use pandoc to "compile" the rst version of the README file
+# before uploading to cheeseshop:
+#
+#     pandoc README.md -o README.rst
+try:
+    long_description = open('README.rst', 'r').read()
+except:  # (IOError, ImportError, OSError, RuntimeError):
+    long_description = description
+    print('WARNING: Unable to find or read README.rst.')
 
 setup(name="nlup",
       include_package_data=True,
-      install_requires=['jsonpickle==0.9.0'],
       version="0.5",
-      description="Core libraries for natural language processing",
+      description=description,
       author="Kyle Gorman",
       author_email="gormanky@ohsu.edu",
       # url="http://github.com/cslu-nlp/nlup/",
