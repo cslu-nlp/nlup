9b47a83cce915874adb4f3262c99a377f11166fd
Hobson Lane <hobsonlane@gmail.com>
2015-05-01 12:31:50 -0700

    prepare for cheese shop

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 
 setup(name="nlup",
+      include_package_data=True,
+      install_requires=['jsonpickle==0.9.0'],
       version="0.5",
       description="Core libraries for natural language processing",
       author="Kyle Gorman",
       author_email="gormanky@ohsu.edu",
-      url="http://github.com/cslu-nlp/nlup/",
+      # url="http://github.com/cslu-nlp/nlup/",
       install_requires=["jsonpickle >= 0.9.0"],
-      packages=["nlup"])
+      packages=["nlup"],
+      )
